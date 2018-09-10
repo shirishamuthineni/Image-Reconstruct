10b97cf7fc43f9567c7f57966b9f342a7bf3d1ec
Muthineni Shirisha <31398849+shirishamuthineni@users.noreply.github.com>
2018-09-10 10:23:20 +0530

    Update padding1.py

--- padding1.py
+++ padding1.py
@@ -6,33 +6,50 @@ Created on Wed Aug 29 16:52:34 2018
 """
 import matplotlib.pyplot as plt
 from scipy import misc
-import numpy as np  
-image = misc.imread('sinogram0_image.png', flatten=True).astype('float64')
-print (image.shape)
+from PIL import Image
+import numpy as np
+import os
+import imageio
+import matplotlib.image as mpimg
 
-res = []
-size = 0
+def createFolder(directory):
+    try:
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+    except OSError:
+        print('Error: Creating directory. '+ directory)
+def padding(image_input):
+    input_row = image_input.shape[0]
+    input_col = image_input.shape[1]
+    #image_input = np.resize(image_input, (input_row, input_col))
 
-
-def paddingO(initial_array, pad_size):
-    in_row, in_col = initial_array.shape
-    print("\----In_columns--------")
-    print(in_col)
-    res_row, res_col = in_row+pad_size*2, in_col+pad_size*2
+    pad_row = (182 - input_row)
+    pad_col =int((182 - input_col)*0.5)
+    #int(pad_col)
+    print("pad_row: ", pad_row)
+    print("pad_col", pad_col)
     
-    #res[1000][1000]
-    res = np.zeros((res_row, res_col))
-    
-    res[size: res_row-2*pad_size, size: res_col-2*pad_size] = initial_array
+    pad_images = np.pad(image_input, ((pad_row, pad_row), (pad_col, pad_col)),  mode = "constant")
+    #res_images = np.resize(pad_images, (pad_images.shape[0], 182))
+    print("\\n---------------", pad_images.shape)
+    return pad_images
 
-    
-    return res
-y_train = misc.imread('phantom128Head.png', flatten=True).astype('float64')
-print("\n--------------------Before padding:---------------")
-plt.imshow(y_train, cmap="gray")
-y_train = paddingO(y_train, 24)
-row, cols = y_train.shape
-print(row)
-print(cols)
-print("\n----------------After Padding-------------")
-plt.imshow(y_train, cmap="gray")
+def rgb2gray(rgb):
+    return np.dot(rgb[...,:3], [0.299, 0.587, 0.114])
+
+path = "./radon_images/"
+createFolder('./paded_images/')
+pad_image = "./paded_images/"
+images = os.listdir(path)
+for file in images:
+    # input_image = Image.open(path+file)
+    input_image = misc.imread(path+file)
+    gray_image = rgb2gray(input_image)  
+    print(gray_image.shape)
+    #plt.imshow(gray_image)
+    pad_images =  padding(gray_image)
+    # pad_images.imsave(pad_image+file)
+    print("\n--------------")
+    print("\n-----------------", pad_images.shape)
+    plt.imshow(pad_images)
+    imageio.imwrite(pad_image+file,pad_images)
